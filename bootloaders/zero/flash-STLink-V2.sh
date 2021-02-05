0d3e5a1ef046194f2c38911ca71e1e1877f1f48d
boseji <boseji@users.noreply.github.com>
2021-02-05 14:31:45 +0530

    fixed Boot config issues for flashing

--- bootloaders/zero/flash-STLink-V2.sh
+++ bootloaders/zero/flash-STLink-V2.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-
-openocd -d2 -s /usr/share/openocd/scripts/ -f interface/stlink-v2.cfg -f atsamd21g18.cfg -c "telnet_port disabled; init; halt; at91samd bootloader 0; program samd21_sam_ba.bin verify reset; shutdown"
+HEXFILE='samd21_sam_ba.hex'
+openocd -d2 -s /usr/share/openocd/scripts/ -f interface/stlink-v2.cfg -f atsamd21g18.cfg -c "telnet_port disabled; init; reset init;at91samd chip-erase; load_image ${HEXFILE} 0 ihex;at91samd bootloader 8192;verify_image ${HEXFILE} 0 ihex; shutdown"
